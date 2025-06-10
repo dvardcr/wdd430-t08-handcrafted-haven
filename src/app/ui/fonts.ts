6f4ee2f2bb22f3603249ac8189ff9102bc2f803b
jacebright <jacebright@gmail.com>
2025-06-09 21:29:44 -0500

    update font

--- src/app/ui/fonts.ts
+++ src/app/ui/fonts.ts
@@ -3,6 +3,7 @@ import { Playfair_Display, Lato } from "next/font/google";
 
 export const Playfair = Playfair_Display({
   variable: "--font-playfair_display",
+  subsets: ["latin"],
 });
 
 export const lato = Lato({
